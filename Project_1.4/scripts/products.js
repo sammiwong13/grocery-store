dc359ea556f9b53fe94255959197b921981c6206
Sammi Wong <sammiwong101302@gmail.com>
2023-10-01 23:56:56 -0400

    button ripple effect

--- Project_1.4/scripts/products.js
+++ Project_1.4/scripts/products.js
@@ -252,5 +252,30 @@ function updateQuantity(elementQty, objectProduct, array) {
     }
 }
 
+//Button ripple effect (udemy day 20)
+let buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
+
 
 
